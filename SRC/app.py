7c91bb6deed5de05ac4c55f3a0e64f663766f472
lauragreemko <laura.barreda@greemko.com>
2022-10-02 17:39:33 +0200

    added encoding to predictions

--- SRC/app.py
+++ SRC/app.py
@@ -16,14 +16,31 @@ def hello():
 # 1. Devolver la predicción de los nuevos datos enviados mediante argumentos en la llamada
 @app.route('/predict', methods=['GET'])
 def predict():
-    model = pickle.load(open('model/model_random','rb'))
+    model = pickle.load(open('model_random','rb'))
 
-    league_id = int(request.args.get('league_id', None))
-    season = int(request.args.get('season', None))
-    home_team_name = int(request.args.get('home_team_name', None))
-    away_team_name = int(request.args.get('away_team_name', None))
+    country = request.args.get('country', None)
+    season = request.args.get('season', None)
+    home_team_name = request.args.get('home_team_name', None)
+    away_team_name = request.args.get('away_team_name', None)
 
-    prediction = model.predict([[league_id,season,home_team_name,away_team_name]])
+    le_country = pickle.load(open('le_country','rb'))
+    le_season = pickle.load(open('le_season','rb'))
+    le_home_team_name = pickle.load(open('le_home_team_name','rb'))
+    le_away_team_name = pickle.load(open('le_away_team_name','rb'))
+
+    country = le_country.transform([country])
+    season = le_season.transform([season])
+    home_team_name = le_home_team_name.transform([home_team_name])
+    away_team_name = le_away_team_name.transform([away_team_name])
+
+    country = country[0]
+    season = season[0]
+    home_team_name = home_team_name[0]
+    away_team_name = away_team_name[0]
+
+    prediction = model.predict([[country, season, home_team_name, away_team_name]])
+    prediction =  str(prediction[0])
+    
     return render_template('predict.html', predict=prediction)
 
     # if league_id is None or season is None or home_team_name or away_team_name is None:
