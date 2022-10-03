000aaf6acffec11f41e4ef78622403edefcdc0eb
lauragreemko <laura.barreda@greemko.com>
2022-10-03 10:36:48 +0200

    added third endpoint

--- SRC/app.py
+++ SRC/app.py
@@ -4,6 +4,9 @@ from flask import Flask, request, render_template, url_for
 import os
 import pickle
 import sqlite3
+import pandas as pd
+from sklearn import metrics
+import json
 
 os.chdir(os.path.dirname(__file__))
 
@@ -47,7 +50,7 @@ def predict():
         
         return render_template('predict.html', predict=prediction)
 
-# 1. Devolver la predicción de los nuevos datos enviados mediante argumentos en la llamada
+# 2. Ingestar nuevos datos
 @app.route('/ingest', methods=['GET'])
 def ingest():
     country = request.args.get('country', None)
@@ -65,6 +68,51 @@ def ingest():
 
     crsr.execute(query, params)
 
+    connection.close()
+
     return 'Data has been added to the database'
+
+# 3. Monitorizar rendimiento
+@app.route('/monitor', methods=['GET'])
+def monitor():
+    model = pickle.load(open('../../big_files/model_random','rb'))
+    le_country = pickle.load(open('../../big_files/le_country','rb'))
+    le_season = pickle.load(open('../../big_files/le_season','rb'))
+    le_home_team_name = pickle.load(open('../../big_files/le_home_team_name','rb'))
+    le_away_team_name = pickle.load(open('../../big_files/le_away_team_name','rb'))
+
+    connection = sqlite3.connect("../../big_files/database.sqlite")
+    crsr = connection.cursor()
+
+    query = '''SELECT * FROM prediction'''
+
+    crsr.execute(query)
+    ans = crsr.fetchall()
+    names = [description[0] for description in crsr.description]
+
+    df = pd.DataFrame(ans,columns=names)
+
+    df['country'] = le_country.transform(df['country'])
+    df['season'] = le_season.transform(df['season'])
+    df['home_team_name'] = le_home_team_name.transform(df['home_team_name'])
+    df['away_team_name'] = le_away_team_name.transform(df['away_team_name'])
+
+    connection.close()
+
+    X = df.drop('result', axis=1)
+    y = df['result']
+
+    prediction = model.predict(X)
+
+    new_accuracy = metrics.accuracy_score(y, prediction)
+
+    f = open('accuracy_monitor.json')
+    dict = json.load(f)
+    accuracy = dict['accuracy_0']
+
+    if new_accuracy < accuracy:
+        return 'The accuracy is not improving, the model needs to be retrained'
+    else:
+        return 'The model does not need to be retrained'
 
 app.run()
